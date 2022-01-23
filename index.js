883fb888d1ec482fa3d0e5c8783c8372552d4669
Tracy Tran <tracytran1189@gmail.com>
2022-01-23 15:24:55 -0600

    create function to writToFile and initialize app

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
 const inquirer = require("inquirer");
+
+
 // TODO: Create an array of questions for user input
 const questions = [{
     type: "input",
@@ -53,10 +56,23 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeToFile(file, data, function(err) {
+        if(err) {
+            return console.log(err)
+        }else {
+            console.log("check out the new README file")
+        }
+    })
+ }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("READMED.md", generatorMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
